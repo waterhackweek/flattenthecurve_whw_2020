bc69efa4854c2726835f5e142d50a6acc74c747f
Matias Garcia Isaia <mgarcia@manas.tech>
2020-03-16 18:23:27 -0300

    WIP - try to generate other pages from translations
    /about/, /contact/
    
    It's not working yet

--- _plugins/common.rb
+++ _plugins/common.rb
@@ -6,3 +6,12 @@ def key_from_filename(name)
       raise "#{basename} doesn't match the \"99-some-file-name.md\" name format (in #{name})"
   end
 end
+
+def key_from_top_level_file(name)
+  basename = File.basename(name)
+  if basename =~ /(.+)-(\w\w).md/
+    return $1
+  else
+    raise "#{basename} doesn't match the \"some-page-en.md\" name format (in #{name})"
+  end
+end
